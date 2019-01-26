019ad30c870e735613ad1618807ed51a629232ad
Audore <audore@kapsi.fi>
2019-01-26 13:31:36 +0200

    Rename SuperScript to HomeScript and add basic dialog for home screen

---
+++ Assets/DialogScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogScript : MonoBehaviour
+{
+    public Text nameText;
+    public Text questionText;
+    public Button buttonYes;
+    public Button buttonNo;
+    private Canvas dialogCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = "Hello hello";
+        questionText.text = "What do you wanna do?";
+        buttonNo.GetComponentInChildren<Text>().text = "No";
+        buttonNo.GetComponentInChildren<Text>().text = "OK";
+        this.dialogCanvas = GameObject.Find("HomeCanvas").GetComponent<Canvas>();
+        Debug.Log(dialogCanvas.enabled);
+        dialogCanvas.enabled = false;
+        Debug.Log(dialogCanvas.enabled);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
