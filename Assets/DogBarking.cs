4bd41977153055e9d784ec9f5743040a44f28422
Karoliina Jäspi <kjaspi@gmail.com>
2019-01-27 12:55:41 +0200

    Fixed sounds + added remppa barking feature

---
+++ Assets/DogBarking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogBarking : MonoBehaviour
+{
+    AudioSource dogBarkingAsrc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dogBarkingAsrc = this.GetComponent<AudioSource>();
+        dogBarkingAsrc.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindWithTag("Inventory").GetComponent<InventoryScript>().dogBarking == false)
+        {
+            dogBarkingAsrc.Stop();
+        }
+    }
+}
